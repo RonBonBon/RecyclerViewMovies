a2ef4c9c1c5fb52ccf9cca6767e1e5174c1a753a
RonBonBon <ron-aricha@013.net>
2017-05-25 19:57:42 +0300

    movie images

--- app/src/main/java/hackeru/edu/recyclerviewmovies/MovieRecyclerAdapter.java
+++ app/src/main/java/hackeru/edu/recyclerviewmovies/MovieRecyclerAdapter.java
@@ -3,7 +3,11 @@ package hackeru.edu.recyclerviewmovies;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class MovieRecyclerAdapter extends RecyclerView.Adapter<MovieViewHolder> {
+
+    List<Movie> movies = MoviesDataSource.getMovies();
 
     @Override
     public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -11,12 +15,13 @@ public class MovieRecyclerAdapter extends RecyclerView.Adapter<MovieViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(MovieViewHolder holder, int position) {
-
+    public void onBindViewHolder(MovieViewHolder h, int position) {
+        Movie m = movies.get(position);
+        h.tvTitle.setText(m.getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movies.size();
     }
 }
