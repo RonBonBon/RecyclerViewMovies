a04237bc78173ddf9a4bceb9b0183af1955f1741
RonBonBon <ron-aricha@013.net>
2017-05-25 19:47:25 +0300

    movie images

---
+++ app/src/main/java/hackeru/edu/recyclerviewmovies/MovieRecyclerAdapter.java
@@ -0,0 +1,22 @@
+package hackeru.edu.recyclerviewmovies;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class MovieRecyclerAdapter extends RecyclerView.Adapter<MovieViewHolder> {
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/hackeru/edu/recyclerviewmovies/MovieViewHolder.java
+++ app/src/main/java/hackeru/edu/recyclerviewmovies/MovieViewHolder.java
@@ -1,8 +1,21 @@
 package hackeru.edu.recyclerviewmovies;
 
-/**
- * Created by hackeru on 25/05/2017.
- */
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MovieViewHolder {
+public class MovieViewHolder extends RecyclerView.ViewHolder {
+
+    TextView tvTitle, tvGenre, tvRating, tvYear;
+    ImageView ivThumbnail;
+
+    public MovieViewHolder(View v) {
+        super(v);
+        tvTitle = (TextView) v.findViewById(R.id.tvTitle);
+        tvGenre = (TextView) v.findViewById(R.id.tvGenre);
+        tvRating = (TextView) v.findViewById(R.id.tvRating);
+        tvYear = (TextView) v.findViewById(R.id.tvYear);
+        ivThumbnail = (ImageView) v.findViewById(R.id.ivThumbnail);
+    }
 }
